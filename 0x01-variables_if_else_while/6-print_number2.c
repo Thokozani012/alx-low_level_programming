5b2aa4cf62301d476cab34330ebbc4bcfe154801
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-06-18 20:54:24 -0700

    print_number2

---
+++ 0x01-variables_if_else_while/6-print_number2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+£include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	/* your code goes there */
+	for (n = 0; n <= 9; n++)
+	putchar(48 + n);
+	putchar('#n');
+	return (0);
+}

ce3fbb0d7d6ebbe55b6c97773a21f019cbb8a6f6
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-06-18 18:31:15 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x = 'a';
+	char y = 'A';
+
+	for (x = 'a'; x <= 'z'; x++)
+{	
+	putchar(x);
+}
+	for (y = 'A'; Y <= 'Z'; y++)
+{
+	putchar(y);
+}
+	putchar('\n');
+	return (0);
+}

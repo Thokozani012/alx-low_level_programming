dca9d2d349ea6780f70044da3b7e7e4da0e6f033
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-06-18 19:18:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 	char x = 'a';
 	char y = 'A';
 
+	/* your code goes there */
 	for (x = 'a'; x <= 'z'; x++)
-{	
 	putchar(x);
-}
 	for (y = 'A'; y <= 'Z'; y++)
-{
-	putchar(y);
-}
+	pucthar(y)l
 	putchar('\n');
 	return (0);
 }

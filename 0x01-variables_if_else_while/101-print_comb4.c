9467c7d16c6dfaf453dc688c4fd89b6c1d6999ef
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-06-20 03:15:33 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print all possible different combinations of three digits
+ * Numbers must be separated by commas and spaces
+ * The three digits must be different
+ * Print only the smallest combination of the three digits
+ * Use only 'putchar' function and only six times
+ * Not allowed to use the variable type of 'char'
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if (a != b && b != c && a != c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c < 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

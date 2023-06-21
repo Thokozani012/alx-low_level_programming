bce2ae4ebfbebc9e590813c961883d16671036b2
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-06-21 06:42:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * Description: Prints all alphabets in lower case.
+ *
+ * Return: 0 (Success)
+ */
+
+void  print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+	return (0);
+}

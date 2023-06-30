f75f3b7669399f92b169a95e8a403943b8b97c8a
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-06-30 01:35:24 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,20 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < size - 1; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - 1; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('#');
+			_putchar('\n');
+		}
 	}
 }

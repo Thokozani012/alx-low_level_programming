a40a9dfe90dd37dbe1981f97f55517993070e99b
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-07-10 02:02:05 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -30,19 +30,16 @@ int main(int argc, char *argv[])
 			cents -= 25;
 			continue;
 		}
-
 		if ((cents - 10) >= 0)
 		{
 			cents -= 10;
 			continue;
 		}
-
-		if((cents - 5) >= 0)
+		if ((cents - 5) >= 0)
 		{
 			cents -= 5;
 			continue;
 		}
-
 		if ((cents - 2) >= 0)
 		{
 			cents -= 2;

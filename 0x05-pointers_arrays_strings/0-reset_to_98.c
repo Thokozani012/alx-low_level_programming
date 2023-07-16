ff9068e8d996dc7e1c181762eec58e4ad105a040
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-07-16 02:26:26 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reset_to_98 - Updates the value to 98
+ * @n: is the parameter to be updated
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

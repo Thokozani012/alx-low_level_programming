68a0e2d57cedcbcbf9dbb89c20cb8e0f96156288
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-07-18 06:49:57 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Defines common variables
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner's dog
+ *
+ * Description: The structure defines teh attributes of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

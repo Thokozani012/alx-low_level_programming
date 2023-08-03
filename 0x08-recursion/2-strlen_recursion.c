8d8d11ac1c7971cb142ba0351bbd6c327a84d331
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-03 00:32:26 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - Determines the length of a string
+ * @s: string
+ *
+ * Return: String length.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}

65472196ab98b484b8180e63428df65e76026c4c
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-04 02:54:49 -0700

    libmy.a_main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string to the stdout
+ * @str: string to be printed
+ *
+ * Return: Nothing (void function)
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}

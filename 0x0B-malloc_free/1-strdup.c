3d1a70f93d354e3994a382951084cc0a02582250
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-09 03:40:15 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,6 +3,51 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Determines the length of a string
+ * @str: String
+ *
+ * Return: string length
+ */
+int _strlen(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * _strcpy - Copies the provided string
+ * @dest: duplicate string
+ * @source: source strng to be duplicated
+ *
+ * Return: S string duplicate
+ */
+
+char *_strcpy(char *dest, const char *source)
+{
+	char *originalDest;
+
+	if (dest == NULL || source == NULL)
+		return (NULL);
+
+	originalDest = dest;
+
+	while (*source != '\0')
+	{
+		*dest = *source;
+		dest++;
+		source++;
+	}
+	*dest  = '\0';
+	return (originalDest);
+}
+
+/**
  * _strdup - Returns a pointer to a new duplicate string
  * @str: string to be duplicated
  *

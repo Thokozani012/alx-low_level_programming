ddc14b46bea50adcc11f7388e6c7d221aea730f3
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-27 23:41:13 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list
+ * @h: Pointer to the singly linked list 'listint'
+ *
+ * Return: Number of nodes (On success)
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *ptr;
+	int count = 0;
+
+	if (h == NULL)
+		return (-1);
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,5 +16,6 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
+size_t print_listint(const listint_t *h);
 
 #endif /* _LISTS_H */

b2ce667bc09bc2390dd06b8c4febe5436991a880
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-09-06 04:47:18 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,26 +19,24 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
+
+	fptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fptr < 0)
+		return (-1);
+	while (text_content[size] != '\0')
 	{
-		fptr = open(filename, O_WRONLY | O_CREAT, 0600);
+		size++;
+	}
+	if (text_content != NULL)
+	{
+		wr = write(fptr, text_content, size);
 		close(fptr);
+
+		if (wr < 0)
+			return (-1);
 	}
 	else
 	{
-		fptr = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
-		if (fptr < 0)
-			return (-1);
-		while (text_content[size] != '\0')
-		{
-			size++;
-		}
-		wr = write(fptr, text_content, size);
-		if (wr < 0)
-		{
-			close(fptr);
-			return (-1);
-		}
 		close(fptr);
 	}
 	return (1);

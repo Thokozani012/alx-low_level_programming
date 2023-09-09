d72cdad3d9f2a4d53966d2218d5607ff8a0bb152
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-09-09 06:10:39 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,19 +11,19 @@
 
 int main(int argc, char *argv[])
 {
-	int fd_s, fd_d, bRead, bWrittern;
+	int file_from, file_to, bRead, bWrittern;
 	char buffer[BUFFER_SIZE];
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
-	fd_s = open(argv[1], O_RDONLY);
-	if (fd_s == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	fd_d = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
-	if (fd_d == -1)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
 	}
@@ -31,21 +31,21 @@ int main(int argc, char *argv[])
 	bRead = 1;
 	while (bRead)
 	{
-		bRead = read(fd_s, buffer, BUFFER_SIZE);
+		bRead = read(file_from, buffer, BUFFER_SIZE);
 		if (bRead == -1)
 			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		if (bRead > 0)
 		{
-			bWrittern = write(fd_d, buffer, bRead);
+			bWrittern = write(file_to, buffer, bRead);
 			if (bWrittern != bRead || bWrittern == -1)
 				dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
 		}
 	}
 
-	if (close(fd_s) == -1)
-		dprintf(2, "Error: Can't close fd file %d\n", fd_s), exit(100);
-	if (close(fd_d) == -1)
-		dprintf(2, "Error: Can't close fd file %d\n", fd_d), exit(100);
+	if (close(file_from) == -1)
+		dprintf(2, "Error: Can't close fd file %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(2, "Error: Can't close fd file %d\n", file_to), exit(100);
 
 	return (0);
 }

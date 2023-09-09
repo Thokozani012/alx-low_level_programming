f6092a58b0fb6a7956b27b2c67e9564fb3b0571f
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-09-09 06:40:00 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,20 +13,18 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, bRead, bWrittern;
 	char buffer[BUFFER_SIZE];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	/*mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;*/
 
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
-	{
-		dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
-	}
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	bRead = 1;
 	while (bRead)

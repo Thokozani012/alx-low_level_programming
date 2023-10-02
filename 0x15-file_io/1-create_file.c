562a9bab6277791b0c4d142f24ba6892289539ce
Thokozani012 <Thokozanidoctor123@gmail.com>
2023-10-02 11:51:34 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wd, len = 0;
+	int fd, wd, len = 0, i;
 	char *buffer;
 
 	if (filename == NULL)
@@ -19,11 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
 		close(fd);
 		return (1);
 	}
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[len] != '\0')
@@ -32,6 +34,10 @@ int create_file(const char *filename, char *text_content)
 	buffer = malloc(sizeof(char) * len + 1);
 	if (buffer == NULL)
 		return (-1);
+
+	for (i = 0; i < len; i++)
+		buffer[i] = text_content[i];
+	buffer[len] = '\0';
 
 	wd = write(fd, buffer, len);
 	if (wd == -1)

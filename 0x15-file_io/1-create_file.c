b7cef3a7a368f74830c5d4772c46b0f812a02204
Thokozani012 <Thokozanidoctor123@gmail.com>
2023-10-02 11:11:58 -0700

    1-create_filec

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file to be created
+ * @text_content: Null terminated string to write to the file
+ *
+ * Return: On success 1, or -1 on error/failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wd, len = 0;
+	char *buffer;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		close(fd);
+		return (1);
+	}
+
+	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content[len] != '\0')
+		len++;
+
+	buffer = malloc(sizeof(char) * len + 1);
+	if (buffer == NULL)
+		return (-1);
+
+	wd = write(fd, buffer, len);
+	if (wd == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (-1);
+	}
+
+	free(buffer);
+	close(fd);
+	return (1);
+}

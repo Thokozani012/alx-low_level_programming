eac9b9597e2d78555115428ec0d271dd65f9c1c6
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-11-27 16:51:32 +0200

    header_file_issue(s)_resolved

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,14 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <elf.h>
 #include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(conts char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 void check_elf(unsigned char *e_ident);

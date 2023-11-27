c7ef3fc8ac492f8713aa99494663e5f5405052fb
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-11-27 11:30:05 +0200

    0x15-file_io

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * append_text_to_file - Appends text at the end of a file
+  * @filename: file to be appended
+  * @text_content: content to be added at the end of 'filename' file
+  *
+  * Return: On success 1, or -1 on error
+  */
+int append_text_to_file(const chat *filename, char *text_content)
+{
+	int fd, wd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+
+	fd == open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[len] != '\0')
+		len++;
+
+	wd = write(fd, text_content, len);
+	if (wd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+  * main - Copies a file to another file
+  * @argc: Argument counter
+  * @argv: argument vector
+  *
+  * Return: 0 on Success, or exit code 97 - 100 on error
+  */
+
+int main(int argc, char *argv[])
+{
+	ssize_t rd_from, wd;
+	int file_from, file_to;
+	mode_t perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	while ((rd_from = read(file_from, buffer, sizeof(buffer))))
+	{
+		if (rd_from == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		}
+		wd = write(file_to, buffer, rd_from);
+		if (wd == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
+	}
+	return (0);
+}

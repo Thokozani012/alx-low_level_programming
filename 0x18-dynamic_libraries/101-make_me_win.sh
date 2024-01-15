8f9be0aaf42c670241ead3eb4c8da8a0ea6f6361
Thokozani012 <Thokozanidoctor123@gmail.com>
2024-01-15 13:24:37 -0800

    101-make_me_win.sh->initial code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e '#include <stdio.h>\n#include <stdlib.h>\nvoid gm(int n1, int n2, int n3, int n4, int n5, int bonus) { printf("--> Please make me win!\\n");}' > /tmp/inject.c
-gcc -shared -fPIC -o /tmp/inject.so /tmp/inject.c
-LD_PRELOAD=/tmp/inject.so ./gm 9 8 10 24 75 9
+echo -e '#!/bin/bash\necho -e "--> Please make me win!" > gm' > /tmp/gm_script
+chmod +x /tmp/gm_script

ce9e9606a3a26ff583f29bddcfca53efdd93bf5f
Thokozani012 <Thokozanidoctor123@gmail.com>
2024-02-09 10:26:09 +0200

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * bianry_to_uint - Converts a binanry number to an unsigned int
+  * @b: binary number to be converted
+  *
+  * Return: converted number, o r0 if b has none (1s and 0s)
+  * or when b is NULL
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, decNum = 0; len = 0;
+
+	if (b == NULL)
+		return (0);
+	
+	while (b[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] == '1')
+		{
+			decNum += (1 << (len - 1 - i));
+		}
+		else if (b[i] != '\0')
+		{
+			return (0);
+		}
+	}
+	return (decNum);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Macros */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Function prototypes */
+int _putchar(char c);
+
+#endif /* MAIN_H */

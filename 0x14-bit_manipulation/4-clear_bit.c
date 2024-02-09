6ce535b432ae7d8fc558d26b532f7081aaca2415
Thokozani012 <Thokozanidoctor123@gmail.com>
2024-02-10 00:51:11 +0200

    4-clear_bit.c->betty error fixed at line 14

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask, nbits;
 
-	nbits = sizeof(unsigned long int ) * 8;
+	nbits = sizeof(unsigned long int) * 8;
 
 	if (index >= nbits)
 		return (-1);

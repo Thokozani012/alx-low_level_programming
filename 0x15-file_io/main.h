525ff918ddfa4d1038945065f7135dc2115a6314
Thokozani012 <Thokozanidoctor123@gmail.com>
2024-02-12 17:36:58 +0200

    header file main.h created*/

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*macros*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*function prototypes*/
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /*MAIN_H*/

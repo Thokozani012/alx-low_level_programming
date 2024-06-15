f5fe8e09091a6c2a33c7c6c96af21e179cb79128
Thokozani012 <Thokozanidoctor123@gmail.com>
2024-06-15 22:45:28 +0200

    1-dlisint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,15 +11,16 @@ size_t dlistint_len(const dlistint_t *h)
 	const dlistint_t *ptr = h;
 	size_t elements = 0;
 
-	if (!h)
-		return (elements);
+	if (h == NULL)
+	{
+		return (0);
+	}
 
 	while (ptr != NULL)
 	{
-		ptr = ptr->next;
 		elements++;
+		ptr = ptr->next;
 	}
-	elements++;
 
 	return (elements);
 }

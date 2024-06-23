54a44bb7c96fe40af5b6d7206f2fbae9a012d8f3
Thokozani012 <Thokozanidoctor12@gmail.com>
2024-06-23 11:19:41 +0200

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Finds and retuns the nth node of a dlistint_t list
+ * @head: head pointer to the list
+ * @index: Positrion of the node to be returned/extracted
+ *
+ * Return: On success the nth node on the list, or NULL node does not exists
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+	unsigned int n = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (ptr->next != NULL && index != n)
+	{
+		ptr = ptr->next;
+		n++;
+	}
+
+	return (ptr);
+}

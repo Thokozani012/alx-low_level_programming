72aea25467aa864ccd6bf3d7e4c0171dc602395a
Thokozani012 <Thokozanidoctor12@gmail.com>
2024-06-30 20:52:01 +0200

    main.h

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/*macrios*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*functions*/
+int _putchar(char c);
+
+#endif /*_MAIN_H*/
